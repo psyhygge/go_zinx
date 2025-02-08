02b9bcbab40f1aa667e10410616144dde696552d
psyhygge <psyhygge@163.com>
2025-02-08 23:11:25 +0800

    服务端

--- zinx_app/server.go
+++ zinx_app/server.go
@@ -15,29 +15,15 @@ type PingRouter struct {
 	znet.BaseRouter
 }
 
-func (p *PingRouter) PreHandle(request ziface.IRequest) {
-	fmt.Println("Call PreHandle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("PreHandle PingRouter"))
-	if err != nil {
-		fmt.Println("PreHandle write conn err ", err)
-		return
-	}
-}
-
 func (p *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call Handle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("Handle PingRouter"))
-	if err != nil {
-		fmt.Println("Handle write conn err ", err)
-		return
-	}
-}
+	// 读取客户端数据
+	// 根据MsgID做不同的业务处理
+	fmt.Println("recv from client: msgID = ", request.GetMsgID(), ", data = ", string(request.GetData()))
 
-func (p *PingRouter) PostHandle(request ziface.IRequest) {
-	fmt.Println("Call PostHandle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("PostHandle PingRouter"))
+	err := request.GetConnection().SendMsg(1, []byte("ping...ping...ping..."))
 	if err != nil {
-		fmt.Println("PostHandle write conn err ", err)
+		fmt.Println("write back err ", err)
 		return
 	}
 }
