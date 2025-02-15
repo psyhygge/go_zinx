4b822b6cca7a753650d8052f3787bfe5f4f746f1
psyhygge <psyhygge@163.com>
2025-02-15 20:22:30 +0800

    客户端

--- zinx_app/client.go
+++ zinx_app/client.go
@@ -8,6 +8,64 @@ import (
 	"time"
 )
 
+// 发送数据的 goroutine
+func writeLoop(conn net.Conn) {
+	defer conn.Close()
+	dp := znet.NewDataPack()
+
+	for {
+		// 调用write写数据
+		binaryMsg, err := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx psyhygge client Test Message")))
+		if err != nil {
+			fmt.Println("client pack err ", err)
+			return
+		}
+
+		if _, err := conn.Write(binaryMsg); err != nil {
+			fmt.Println("client write err ", err)
+			return
+		}
+
+		// CPU 阻塞，避免写过快
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// 接收数据的 goroutine
+func readLoop(conn net.Conn) {
+	defer conn.Close()
+	dp := znet.NewDataPack()
+
+	for {
+		// 接收服务器的数据
+		binaryHead := make([]byte, dp.GetHeadLen())
+		_, err := io.ReadFull(conn, binaryHead)
+		if err != nil {
+			fmt.Println("client read head err ", err)
+			return
+		}
+
+		// 解包消息头
+		msg, err := dp.Unpack(binaryHead)
+		if err != nil {
+			fmt.Println("client unpack err ", err)
+			return
+		}
+
+		var data []byte
+		if msg.GetMsgLen() > 0 {
+			// 读取消息体数据
+			data = make([]byte, msg.GetMsgLen())
+			if _, err := io.ReadFull(conn, data); err != nil {
+				fmt.Println("client read msg data err ", err)
+				return
+			}
+		}
+		msg.SetData(data)
+		fmt.Printf("client recv server msg: %s\n", msg.GetData())
+	}
+}
+
 func main() {
 	fmt.Println("client start")
 
@@ -20,44 +78,51 @@ func main() {
 		return
 	}
 
-	for {
-		// 调用write写数据
-		dp := znet.NewDataPack()
-		binaryMsg, err := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx psyhygge client Test Message")))
-		if err != nil {
-			fmt.Println("client pack err ", err)
-			return
-		}
+	// 启动写数据的 goroutine
+	go writeLoop(conn)
 
-		if _, err = conn.Write(binaryMsg); err != nil {
-			fmt.Println("client write err ", err)
-			return
-		}
+	// 启动读数据的 goroutine
+	go readLoop(conn)
 
-		// 接收服务器的数据
-		binaryHead := make([]byte, dp.GetHeadLen())
-		_, err = io.ReadFull(conn, binaryHead)
-		if err != nil {
-			fmt.Println("client read head err ", err)
-			return
-		}
-		msg, err := dp.Unpack(binaryHead)
-		if err != nil {
-			fmt.Println("client unpack err ", err)
-			return
-		}
-		var data []byte
-		if msg.GetMsgLen() > 0 {
-			data = make([]byte, msg.GetMsgLen())
-			if _, err := io.ReadFull(conn, data); err != nil {
-				fmt.Println("client read msg data err ", err)
-				return
-			}
-		}
-		msg.SetData(data)
-		fmt.Printf("client recv server msg: %s\n", msg.GetData())
-
-		// cpu 阻塞
-		time.Sleep(1 * time.Second)
-	}
+	select {}
+	//for {
+	//	// 调用write写数据
+	//	dp := znet.NewDataPack()
+	//	binaryMsg, err := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx psyhygge client Test Message")))
+	//	if err != nil {
+	//		fmt.Println("client pack err ", err)
+	//		return
+	//	}
+	//
+	//	if _, err = conn.Write(binaryMsg); err != nil {
+	//		fmt.Println("client write err ", err)
+	//		return
+	//	}
+	//
+	//	// 接收服务器的数据
+	//	binaryHead := make([]byte, dp.GetHeadLen())
+	//	_, err = io.ReadFull(conn, binaryHead)
+	//	if err != nil {
+	//		fmt.Println("client read head err ", err)
+	//		return
+	//	}
+	//	msg, err := dp.Unpack(binaryHead)
+	//	if err != nil {
+	//		fmt.Println("client unpack err ", err)
+	//		return
+	//	}
+	//	var data []byte
+	//	if msg.GetMsgLen() > 0 {
+	//		data = make([]byte, msg.GetMsgLen())
+	//		if _, err := io.ReadFull(conn, data); err != nil {
+	//			fmt.Println("client read msg data err ", err)
+	//			return
+	//		}
+	//	}
+	//	msg.SetData(data)
+	//	fmt.Printf("client recv server msg: %s\n", msg.GetData())
+	//
+	//	// cpu 阻塞
+	//	time.Sleep(1 * time.Second)
+	//}
 }
